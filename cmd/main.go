226756f65a22b5d9b7a7a330de62f645941ae86e
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-02-23 17:02:21 +0200

    Added stroke option

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,11 @@ import (
 
 func main() {
 	grid := Hexago.MakeHexGrid(1000, 1000, 6, 6)
-	grid.SetRGBA(2, 2, 1, 0, 0, 1)
-	grid.SetRGBA(2, 3, 0, 1, 0, 1)
-	grid.SetRGBA(3, 3, 0, 0, 1, 1)
+	grid.SetFill(2, 2, 1, 0, 0, 1)
+	grid.SetStroke(2, 2, 0, 0, 0, 1, 10)
+	grid.SetFill(2, 3, 0, 1, 0, 1)
+	grid.SetStroke(2, 3, 0, 0, 0, 1, 10)
+	grid.SetFill(3, 3, 0, 0, 1, 1)
+	grid.SetStroke(3, 3, 0, 0, 0, 1, 10)
 	grid.SavePNG("out.png")
 }
--- grid.go
+++ grid.go
@@ -8,15 +8,24 @@ import (
 )
 
 type Hexagon struct {
-	R, G, B, A float64
-	X, Y       int
+	FR, FG, FB, FA     float64
+	SR, SG, SB, SA, SW float64
+	X, Y               int
 }
 
-func (h *Hexagon) SetRGBA(r, g, b, a float64) {
-	h.R = r
-	h.G = g
-	h.B = b
-	h.A = a
+func (h *Hexagon) SetFill(r, g, b, a float64) {
+	h.FR = r
+	h.FG = g
+	h.FB = b
+	h.FA = a
+}
+
+func (h *Hexagon) SetStroke(r, g, b, a, w float64) {
+	h.SR = r
+	h.SG = g
+	h.SB = b
+	h.SA = a
+	h.SW = w
 }
 
 type HexGrid struct {
@@ -27,6 +36,7 @@ type HexGrid struct {
 	Radius        float64
 }
 
+// MakeHexGrid creates a <w> wide and <h> high grid with <r> rows and <c> columns
 func MakeHexGrid(w, h, r, c int) *HexGrid {
 	grid := &HexGrid{
 		Context: gg.NewContext(w, h),
@@ -46,30 +56,46 @@ func MakeHexGrid(w, h, r, c int) *HexGrid {
 	return grid
 }
 
-func (grid *HexGrid) SetRGBA(x, y int, r, g, b, a float64) error {
+// SetFill sets the fill of a cell
+func (grid *HexGrid) SetFill(x, y int, r, g, b, a float64) error {
 	if x >= grid.Rows || y >= grid.Cols {
 		return fmt.Errorf("index error: index [%v][%v] out of bounds (%v,%v)", x, y, grid.Rows, grid.Cols)
 	}
-	grid.Tiles[x][y].SetRGBA(r, g, b, a)
+	grid.Tiles[x][y].SetFill(r, g, b, a)
 	return nil
 }
 
+// SetStroke sets the stroke of a cell
+func (grid *HexGrid) SetStroke(x, y int, r, g, b, a, w float64) error {
+	if x >= grid.Rows || y >= grid.Cols {
+		return fmt.Errorf("index error: index [%v][%v] out of bounds (%v,%v)", x, y, grid.Rows, grid.Cols)
+	}
+	grid.Tiles[x][y].SetStroke(r, g, b, a, w)
+	return nil
+}
+
+// SavePNG saves the grid as a PNG
 func (grid *HexGrid) SavePNG(path string) error {
 	height := math.Sqrt(3 * grid.Radius * grid.Radius)
 	for i := range grid.Tiles {
 		for j := range grid.Tiles[i] {
 			hex := grid.Tiles[i][j]
+			var x, y float64
 			if j%2 == 0 {
-				x := grid.Radius + (float64(j) * 1.5 * grid.Radius)
-				y := height + (height * float64(i))
+				x = grid.Radius + (float64(j) * 1.5 * grid.Radius)
+				y = height + (height * float64(i))
 				grid.Context.DrawRegularPolygon(6, x, y, grid.Radius, 0)
 			} else {
-				x := grid.Radius + (float64(j) * 1.5 * grid.Radius)
-				y := (height / 2) + (height * float64(i))
-				grid.Context.DrawRegularPolygon(6, x, y, grid.Radius, 0)
+				x = grid.Radius + (float64(j) * 1.5 * grid.Radius)
+				y = (height / 2) + (height * float64(i))
 			}
-			grid.Context.SetRGBA(hex.R, hex.G, hex.B, hex.A)
+			grid.Context.DrawRegularPolygon(6, x, y, grid.Radius, 0)
+			grid.Context.SetRGBA(hex.FR, hex.FG, hex.FB, hex.FA)
 			grid.Context.Fill()
+			grid.Context.DrawRegularPolygon(6, x, y, grid.Radius, 0)
+			grid.Context.SetRGBA(hex.SR, hex.SG, hex.SB, hex.SA)
+			grid.Context.SetLineWidth(hex.SW)
+			grid.Context.Stroke()
 		}
 	}
 	return grid.Context.SavePNG(path)
