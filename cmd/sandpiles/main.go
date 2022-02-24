10a1dc49d6c66deb4fb901ecf8001588e2650ded
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-02-24 16:04:26 +0200

    Added sandpiles exmaple

--- cmd/sandpiles/main.go
+++ cmd/sandpiles/main.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 
 	"github.com/SHA65536/Hexago"
+	"github.com/schollz/progressbar/v3"
 )
 
-const rows, cols int8 = 17, 21
+// Size of board
+const rows, cols int8 = 35, 41
+
+// Number of images to generate
+const frames int = 10000
 
 type color struct {
 	R float64
@@ -15,12 +20,12 @@ type color struct {
 }
 
 var colours = map[int8]color{
-	0: {0.3, 0.3, 0.3},                              // Grey?
-	1: {51.0 / 255.0, 62.0 / 255.0, 212.0 / 255.0},  // Blue?
-	2: {47.0 / 255.0, 162.0 / 255.0, 54.0 / 255.0},  // Green?
-	3: {238.0 / 255.0, 222.0 / 255.0, 4.0 / 255.0},  // Yellow?
-	4: {247.0 / 255.0, 105.0 / 255.0, 21.0 / 255.0}, // Orange?
-	5: {253.0 / 255.0, 1.0 / 255.0, 0},              // Red?
+	0: {0.3, 0.3, 0.3},                             // Grey?
+	1: {51.0 / 255.0, 62.0 / 255.0, 212.0 / 255.0}, // Blue?
+	2: {47.0 / 255.0, 162.0 / 255.0, 54.0 / 255.0}, // Green?
+	3: {1, 1, 0},                                   // Yellow?
+	4: {1, 140.0 / 255.0, 0},                       // Orange?
+	5: {253.0 / 255.0, 1.0 / 255.0, 0},             // Red?
 }
 
 func main() {
@@ -29,35 +34,49 @@ func main() {
 	board := [rows][cols]int8{}
 	grid.SetStrokeAll(0, 0, 0, 1, 0.5)
 
-	for x := 0; x < 1000; x++ {
+	// Creating progress bar
+	bar := progressbar.Default(int64(frames))
+	for x := 0; x < frames; x++ {
+		bar.Add(1)
+
+		// Adding a grain of sand to the center
 		board[rows/2][cols/2] += 1
 		solveBoard(grid, &board)
-		// Applying the  function for each cell
+
+		// Applying the draw function for each cell
 		grid.DrawFunc(func(i, j, _, _ int, _ *Hexago.Hexagon) error {
 			color := colours[board[i][j]]
-			return grid.SetFill(i, j, color.R, color.B, color.G, 1)
+			return grid.SetFill(i, j, color.R, color.G, color.B, 1)
 		})
-		grid.SavePNG(fmt.Sprintf("output/%v.png", x))
+		grid.SavePNG(fmt.Sprintf("output/%07d.png", x))
 	}
 }
 
+// solveBoard takes in a sandpile board and topples the piles
+// until the board is static
 func solveBoard(g *Hexago.HexGrid, b *[rows][cols]int8) {
 	unsolved := true
 	for unsolved {
+		// Buffer to save the sand that falls
 		buffer := [rows][cols]int8{}
+
 		unsolved = false
 		g.DrawFunc(func(i, j, _, _ int, _ *Hexago.Hexagon) error {
+			// If a cell has 6 or more grains, topple it
 			if b[i][j] > 5 {
 				unsolved = true
 				count := b[i][j] / 6
+				// The cell losses all it's sand except the division reminder
 				buffer[i][j] += (-6 * count)
 				around, _ := g.GetNeighbors(i, j)
+				// Each neighbor gets 1/6 of the cell's sand
 				for _, cell := range around {
 					buffer[cell.X][cell.Y] += count
 				}
 			}
 			return nil
 		})
+		// Applying buffer to the board
 		for i := range buffer {
 			for j := range buffer[i] {
 				b[i][j] += buffer[i][j]
--- go.mod
+++ go.mod
@@ -5,5 +5,15 @@ go 1.17
 require (
 	github.com/fogleman/gg v1.3.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/schollz/progressbar/v3 v3.8.6
 	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
+)
+
+require (
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/sys v0.0.0-20220224120231-95c6836cb0e7 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
